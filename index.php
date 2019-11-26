2998e7bd62409892c1fbb4f5ac18866efb8984ff
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-11-26 09:31:47 +0000

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ include('inc/functions.php');
             <div id="login_form">
                 <div class="login-box-body">
                     <center>
-                        <h3>Admin Login</h3>
+                        <h3>Affiliate Login</h3>
                     </center>
 
                     <p class="login-box-msg">Sign in</p>
