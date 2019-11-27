8cdd5dad7653208249c831c957c932f1761dd8ae
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-11-27 20:53:50 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 LOG=/tmp/ubloclub_dashboard.log
 
-VERSION=$(cat /home2/jamie/public_html/projects/ubloclub_dashboard/version.txt)
+VERSION=$(cat /home2/ukmarketingclub/public_html/version.txt)
 
 echo "UBLO Club Affiliate Network - Update Script v"$VERSION
 
@@ -10,8 +10,8 @@ echo "UBLO Club Affiliate Network - Update Script v"$VERSION
 # git remote set-url origin https://github.com/whittinghamj/slistream_cms_production.git
 
 # bash git update script
-cd /home2/jamie/public_html/projects/ubloclub_dashboard >> $LOG
-git --git-dir=/home2/jamie/public_html/projects/ubloclub_dashboard/.git pull -q origin master >> $LOG
+cd /home2/ukmarketingclub/public_html >> $LOG
+git --git-dir=/home2/ukmarketingclub/public_html/.git pull -q origin master >> $LOG
 
 echo "Update Complete"
 echo " "
