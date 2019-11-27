b1d536a0db9399dee3d87f4fd4562e37b3770fe5
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-11-27 20:38:08 +0000

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -946,7 +946,7 @@ desired effect
 												echo '
 													<div class="col-sm-2">
 														<center>
-															<img src="badges/'.$badge['image'].'" alt="'.$badge['description'].'"> <br>
+															<img src="badges/'.$badge['image'].'" alt="'.$badge['name'].'" data-toggle="tooltip" data-placement="bottom" title="'.$badge['description'].'"> <br>
 															<h4><strong>'.stripslashes($badge['name']).'</strong></h4>
 														</center>
 													</div>
