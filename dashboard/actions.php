893dbaf5c9710119dcb4bbf9dd3b58b9661d0d9d
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-12-12 16:43:01 +0000

    Update actions.php

--- dashboard/actions.php
+++ dashboard/actions.php
@@ -2667,10 +2667,10 @@ function my_account_update()
 	$affiliate_username 	= addslashes($affiliate_username);
 
 	// check username availability
-	$query 					= $conn->query("SELECT `id` FROM `users` WHERE `username` = '".$username."' AND `id` != '".$_SESSION['account']['id']."' ");
+	$query 					= $conn->query("SELECT `id` FROM `users` WHERE `affiliate_username` = '".$affiliate_username."' AND `id` != '".$_SESSION['account']['id']."' ");
 	$username_check 		= $query->fetch(PDO::FETCH_ASSOC);
-	if(!isset($username_check['id'])){
-		status_message('danger', "The username '".$username."' you want is already taken, please try something different.");
+	if(isset($username_check['id'])){
+		status_message('danger', "Username is already taken, please try something else.");
 		go($_SERVER['HTTP_REFERER']);
 	}
 
