83b6d904fad62423794fef9a3833fe34e989269f
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-12-20 10:45:23 +0000

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -849,6 +849,7 @@ desired effect
 		              				</h3>
 		            			</div>
 								<div class="box-body">
+									<?php debug($orders); ?>
 									<table id="purchase_history" class="table table-bordered table-striped">
 										<thead>
 											<tr>
