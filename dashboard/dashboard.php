b02f23d634fa808f55bb582f54db0fedca4b8e78
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-12-22 00:18:54 +0000

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -859,7 +859,7 @@ desired effect
 										<thead>
 											<tr>
 												<th class="no-sort" style="white-space: nowrap;" width="1px">Points</th>
-												<th class="no-sort" style="white-space: nowrap;" width="150px">Date</th>
+												<th class="no-sort" style="white-space: nowrap;" width="1px">Date</th>
 												<th class="no-sort" style="white-space: nowrap;">Item(s)</th>
 												<th class="no-sort" style="white-space: nowrap;" width="1px">Amount</th>
 												<th class="no-sort" width="1px">Payment</th>
