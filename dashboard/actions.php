2a282ea4a581670249ee50ad12968cf534621d5a
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-12-22 00:33:22 +0000

    Update actions.php

--- dashboard/actions.php
+++ dashboard/actions.php
@@ -5050,7 +5050,7 @@ function ajax_downline()
 	}
 
 	// downline totals
-	$query 				= $conn->query("SELECT `id`,`status`,`first_name`,`last_name`,`email`,`expire_date`,`internal_notes`,`upline_id`,`total_downline` FROM `users` ");
+	$query 				= $conn->query("SELECT `id`,`added`,`status`,`first_name`,`last_name`,`email`,`expire_date`,`internal_notes`,`upline_id`,`total_downline` FROM `users` ");
 	$customers 			= $query->fetchAll(PDO::FETCH_ASSOC);
 
 	// set defaults
@@ -5175,6 +5175,8 @@ function ajax_downline()
 							break;
 						}
 					}
+
+					$output[$count]['join_date']					= date("Y-m-d", $customer['added']);
 
 					$output[$count]['actions'] 						= '<a title="View / Edit" class="btn btn-info btn-flat btn-xs" href="dashboard.php?c=customer&customer_id='.$customer['id'].'"><i class="fa fa-eye"></i></a><a title="Delete" class="btn btn-danger btn-flat btn-xs" onclick="return confirm(\'This cannot be undone. The entire downline will be moved up one level. Are you sure?\')" href="actions.php?a=customer_delete&customer_id='.$customer['id'].'"><i class="fa fa-times"></i></a>';
 
