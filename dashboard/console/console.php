d50417879c63f58673756dd7f329b5cbbd2b9003
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-01 21:19:55 +0000

    Update console.php

--- dashboard/console/console.php
+++ dashboard/console/console.php
@@ -252,6 +252,46 @@ if($task == 'sync_databases'){
 		$update = $conn->exec("UPDATE `users` SET `first_name` = '".addslashes($user['firstname'])."' WHERE `id` = '".$user['id']."' ");
 		$update = $conn->exec("UPDATE `users` SET `last_name` = '".addslashes($user['lastname'])."' WHERE `id` = '".$user['id']."' ");
 		$update = $conn->exec("UPDATE `users` SET `email` = '".addslashes($user['email'])."' WHERE `id` = '".$user['id']."' ");
+
+		console_output(" -> Getting Products");
+
+		// Set post values
+		$postfields = array(
+		    'username' => $username,
+		    'password' => $password,
+		    'action' => 'GetClientsProducts',
+		    'clientid' => $user['id'],
+		    'limitnum' => '10000',
+		    'responsetype' => 'json',
+		);
+
+		// Call the API
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $whmcsUrl . 'includes/api.php');
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postfields));
+		$response = curl_exec($ch);
+		if (curl_error($ch)) {
+		    die('Unable to connect: ' . curl_errno($ch) . ' - ' . curl_error($ch));
+		}
+		curl_close($ch);
+
+		// Decode response
+		$results = json_decode($response, true);
+
+		// cycle products for qualifying product
+		foreach($results['products']['product'] as $product){
+			// find the right product
+			if($product['pid'] == 1){
+				console_output(" - -> Qualifying Product Found.");
+			}
+		}
+
+
 	}
 
 	console_output("Finished.");
