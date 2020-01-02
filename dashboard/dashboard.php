67cdc4a3cb8e72380ff7b88e898e4ff0d208c96b
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-02 12:28:17 +0000

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -2045,39 +2045,34 @@ desired effect
                 <section class="content-header">
                     <h1>Marketing Tools <!-- <small>Optional description</small> --></h1>
                     <ol class="breadcrumb">
-                        <li class="active"><a href="dashboard.php">Dashboard</a></li>
+                        <li><a href="dashboard.php">Dashboard</a></li>
                         <li class="active">Marketing Tools</li>
                     </ol>
                 </section>
 
-                <!-- Main content -->
-				<section class="content">
-					<div class="row">
-						<div class="col-lg-12">
-							<div class="box box-primary">
+                <section class="content">
+                	<div class="row">
+						<div class="col-lg-4">
+	                        <div class="box box-primary">
 		            			<div class="box-header">
 		              				<h3 class="box-title">
 		              					Affiliate Links
 		              				</h3>
 		            			</div>
 								<div class="box-body">
-									<div class="row">
-										<div class="col-lg-4">
-											<div class="form-group">
-		                                        <label for="affiliate_link_1" class="col-sm-3 control-label">Basic Link</label>
-		                                        <div class="col-sm-9">
-		                                            <input type="text" name="affiliate_link_1" id="affiliate_link_1" class="form-control" value="https://ukmarketingclub.com/aff.php?username=<?php echo $account_details['affiliate_username']; ?>">
-		                                            <small></small>
-		                                        </div>
-		                                    </div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
-            </div>
+	                                <div class="form-group">
+	                                    <label for="affiliate_link_1" class="col-sm-3 control-label">Basic</label>
+	                                    <div class="col-sm-9">
+	                                        <input type="text" name="affiliate_link_1" id="affiliate_link_1" class="form-control" value="https://ukmarketingclub.com/aff.php?username=<?php echo $account_details['affiliate_username']; ?>">
+	                                        <small></small>
+	                                    </div>
+	                                </div>
+	                            </div>
+	                        </div>
+	                    </div>
+	                </div>
+	            </section>
+	        </div>
         <?php } ?>
 
         <?php function staging(){ ?>
