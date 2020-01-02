71d54c1f0cac332353ba76bb4b81c85c1d2e6148
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-02 06:59:24 +0000

    Update actions.php

--- dashboard/actions.php
+++ dashboard/actions.php
@@ -5928,8 +5928,10 @@ function ajax_commissions()
 		// status
 		if($commission['status'] == 'approved') {
 			$output[$count]['status'] 					= '<span class="label label-warning full-width" style="width: 100%;">Approved</span>';
-		}elseif($commission['status'] == 'pending') {
+		}elseif($commission['status'] == 'pending' && $commission['qualified'] == 'yes'){
 			$output[$count]['status']					= '<span class="label label-info full-width" style="width: 100%;">Pending</span>';
+		}elseif($commission['status'] == 'pending' && $commission['qualified'] == 'no'){
+			$output[$count]['status']					= '<span class="label label-default full-width" style="width: 100%;">N/A</span>';
 		}elseif($commission['status'] == 'paid') {
 			$output[$count]['status'] 					= '<span class="label label-success full-width" style="width: 100%;">Paid</span>';
 		}elseif($commission['status'] == 'rejected') {
