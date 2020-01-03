593a541f20929cf8e8f549612aa81a4fe9950b2d
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-03 22:21:06 +0000

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -2061,9 +2061,17 @@ desired effect
 		            			</div>
 								<div class="box-body">
 	                                <div class="form-group">
-	                                    <label for="affiliate_link_1" class="col-sm-3 control-label">Basic</label>
+	                                    <label for="affiliate_link_1" class="col-sm-3 control-label">Basic ublo.club</label>
 	                                    <div class="col-sm-9">
-	                                        <input type="text" name="affiliate_link_1" id="affiliate_link_1" class="form-control" value="https://ukmarketingclub.com/aff.php?username=<?php echo $account_details['affiliate_username']; ?>">
+	                                        <input type="text" name="affiliate_link_1" id="affiliate_link_1" class="form-control" value="https://ublo.club/aff.php?username=<?php echo $account_details['affiliate_username']; ?>">
+	                                        <small></small>
+	                                    </div>
+	                                </div>
+
+	                                <div class="form-group">
+	                                    <label for="affiliate_link_2" class="col-sm-3 control-label">Basic ukmarketingclub.com</label>
+	                                    <div class="col-sm-9">
+	                                        <input type="text" name="affiliate_link_2" id="affiliate_link_2" class="form-control" value="https://ukmarketingclub.com/aff.php?username=<?php echo $account_details['affiliate_username']; ?>">
 	                                        <small></small>
 	                                    </div>
 	                                </div>
