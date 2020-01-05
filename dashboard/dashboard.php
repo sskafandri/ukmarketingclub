2b5871675612f3467cd86809c00efc2741e161df
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-05 04:09:19 +0000

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1855,8 +1855,9 @@ desired effect
 							<div class="box box-warning">
 		            			<div class="box-header">
 		              				<h3 class="box-title">
-		              					Pending
+		              					Approved
 		              				</h3>
+		              				<small>Pending Payment</small>
 		            			</div>
 								<div class="box-body">
 									<center>
