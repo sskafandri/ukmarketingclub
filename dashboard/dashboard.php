8180220d094e5485bf87260f4c886a64326c8ecd
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-09 20:31:58 +0000

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -2498,17 +2498,10 @@ desired effect
 		              					Commissions
 		              				</h3>
 		              				<div class="pull-right">
-		              					<div class="row">
-			              					<div class="col-lg-11 text-center">
-				              					<button id="search_missed" type="button" class="btn btn-danger btn-xs btn-flat" >Missed</button>
-												<button id="search_pending" type="button" class="btn btn-warning btn-xs btn-flat" >Pending</button>
-												<button id="search_paid" type="button" class="btn btn-success btn-xs btn-flat" >Paid</button>
-												<button id="search_reset" type="button" class="btn btn-info btn-xs btn-flat" >Reset Filter</button>
-											</div>
-											<div class="col-lg-1">
-												<button id="search_reset" type="button" class="btn btn-success btn-xs btn-flat" >Request Payout</button>
-											</div>
-										</div>
+		              					<button id="search_missed" type="button" class="btn btn-danger btn-xs btn-flat" >Missed</button>
+										<button id="search_pending" type="button" class="btn btn-warning btn-xs btn-flat" >Pending</button>
+										<button id="search_paid" type="button" class="btn btn-success btn-xs btn-flat" >Paid</button>
+										<button id="search_reset" type="button" class="btn btn-info btn-xs btn-flat" >Reset Filter</button>
 									</div>
 		            			</div>
 								<div class="box-body">
