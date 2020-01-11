f2636ff9a812cebdbba86a0ff8c421f94b29660f
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-11 15:50:12 +0000

    Update actions.php

--- dashboard/actions.php
+++ dashboard/actions.php
@@ -5339,14 +5339,14 @@ function ajax_downline()
 							<tr>
 					            <td width="150px" valign="top">Additional Details</td>
 					            <td valign="top">
-					            	<strong>User ID:</strong>d.id+<br>
-					            	<strong>Join Date:</strong>d.join_date<br>
+					            	<strong>User ID:</strong>'.$output[$count]['id'].'<br>
+					            	<strong>Join Date:</strong>'.$output[$count]['join_date'].'<br>
 					            </td>
 					        </tr>
 					        <tr>
 					            <td width="150px" valign="top">Contact Details</td>
 					            <td valign="top">
-					            	<strong>Email:</strong>d.email<br>
+					            	<strong>Email:</strong>'.$output[$count]['email'].'<br>
 					            </td>
 					        </tr>
 						';
