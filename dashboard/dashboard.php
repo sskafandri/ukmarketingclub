c1b5a413fb7f2766a636e22b9036e0d4a69428bc
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-11 16:41:08 +0000

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -2520,7 +2520,7 @@ desired effect
 
 					<div class="row">
 						<div class="col-lg-6">
-							<button type="button" class="btn btn-info btn-flat full-width" >View withdrawal Requests</button>
+							<button type="button" class="btn btn-info btn-flat full-width" >View Withdrawal Requests</button>
 						</div>
 						<div class="col-lg-6">
 							<button type="button" class="btn btn-success btn-flat full-width" >Request Payout</button>
