3b83ed2b312e8a3881c121f5bf74178ff5b180b4
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-11 15:10:44 +0000

    added additional search filters to all_commissions

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -1940,10 +1940,12 @@ desired effect
 		              					View All Commissions
 		              				</h3>
 		              				<div class="pull-right">
+		              					<button id="search_reset" type="button" class="btn btn-info btn-xs btn-flat" >Show All</button>
+		              					<button id="search_pending" type="button" class="btn btn-warning btn-xs btn-flat" >Pending</button>
+		              					<button id="search_approved" type="button" class="btn btn-warning btn-xs btn-flat" >Approved</button>
+		              					<button id="search_paid" type="button" class="btn btn-success btn-xs btn-flat" >Paid</button>
 		              					<button id="search_missed" type="button" class="btn btn-danger btn-xs btn-flat" >Missed</button>
-										<button id="search_pending" type="button" class="btn btn-warning btn-xs btn-flat" >Pending</button>
-										<button id="search_paid" type="button" class="btn btn-success btn-xs btn-flat" >Paid</button>
-										<button id="search_reset" type="button" class="btn btn-info btn-xs btn-flat" >Reset Filter</button>
+		              					<button id="search_rejected" type="button" class="btn btn-danger btn-xs btn-flat" >Rejected</button>
 									</div>
 		            			</div>
 								<div class="box-body">
@@ -3705,8 +3707,16 @@ desired effect
 					table.search("pending").draw();
 				});
 
+				$("#search_approved").click(function() {
+					table.search("approved").draw();
+				});
+
 				$("#search_missed").click(function() {
 					table.search("missed").draw();
+				});
+
+				$("#search_rejected").click(function() {
+					table.search("rejected").draw();
 				});
 
 				$("#search_paid").click(function() {
