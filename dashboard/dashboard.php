a62eeb1b6987c1336b6a62c8e34c6be142c92b7d
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-12 21:02:07 +0000

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -2083,7 +2083,7 @@ desired effect
 														<div class="form-group">
 															<label class="col-md-2 control-label" for="description">Description</label>
 															<div class="col-md-10">
-																<textarea id="description" name="description" rows="20" style="width: 100%;">
+																<textarea id="description" name="description" rows="40" style="width: 100%;">
 																	<?php echo stripslashes($product['description']); ?>
 											                    </textarea>
 															</div>
