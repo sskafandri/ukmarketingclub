efe5e5ff6206c8121884a9afae8a3795e11460aa
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-12 18:00:21 +0000

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -2056,25 +2056,30 @@ desired effect
 				window.onload = function () {
 				    var nodes = [
 				        // level 0
-		                    { id: "1", Name: "<?php echo $account_details['first_name']; ?> <?php echo $account_details['last_name']; ?>", Level: "Level 0", email: "<?php echo $account_details['email']; ?>", img: "<?php echo $account_details['avatar']; ?>" },
-		                    <?php $downline[1][]=$_SESSION['account']['id']; ?>
-				        { id: 2, pid: 1, name: "Lexie Cole", title: "QA Lead", email: "ava@domain.com", img: "https://cdn.balkan.app/shared/2.jpg" },
-				        { id: 3, pid: 1, name: "Janae Barrett", title: "Technical Director", img: "https://cdn.balkan.app/shared/3.jpg" },
-				        { id: 4, pid: 1, name: "Aaliyah Webb", title: "Manager", email: "jay@domain.com", img: "https://cdn.balkan.app/shared/4.jpg" },
-				        { id: 5, pid: 2, name: "Elliot Ross", title: "QA", img: "https://cdn.balkan.app/shared/5.jpg" },
-				        { id: 6, pid: 2, name: "Anahi Gordon", title: "QA", img: "https://cdn.balkan.app/shared/6.jpg" },
-				        { id: 7, pid: 2, name: "Knox Macias", title: "QA", img: "https://cdn.balkan.app/shared/7.jpg" },
-				        { id: 8, pid: 3, name: "Nash Ingram", title: ".NET Team Lead", email: "kohen@domain.com", img: "https://cdn.balkan.app/shared/8.jpg" },
-				        { id: 9, pid: 3, name: "Sage Barnett", title: "JS Team Lead", img: "https://cdn.balkan.app/shared/9.jpg" },
-				        { id: 10, pid: 8, name: "Alice Gray", title: "Programmer", img: "https://cdn.balkan.app/shared/10.jpg" },
-				        { id: 11, pid: 8, name: "Anne Ewing", title: "Programmer", img: "https://cdn.balkan.app/shared/11.jpg" },
-				        { id: 12, pid: 9, name: "Reuben Mcleod", title: "Programmer", img: "https://cdn.balkan.app/shared/12.jpg" },
-				        { id: 13, pid: 9, name: "Ariel Wiley", title: "Programmer", img: "https://cdn.balkan.app/shared/13.jpg" },
-				        { id: 14, pid: 4, name: "Lucas West", title: "Marketer", img: "https://cdn.balkan.app/shared/14.jpg" },
-				        { id: 15, pid: 4, name: "Adan Travis", title: "Designer", img: "https://cdn.balkan.app/shared/15.jpg" },
-				        { id: 16, pid: 4, name: "Alex Snider", title: "Sales Manager", img: "https://cdn.balkan.app/shared/16.jpg" }
-				    ];
+	                    { id: "1", Name: "<?php echo $account_details['first_name']; ?> <?php echo $account_details['last_name']; ?>", Level: "Level 0", email: "<?php echo $account_details['email']; ?>", img: "<?php echo $account_details['avatar']; ?>" },
+		                <?php $downline[1][]=$_SESSION['account']['id']; ?>
 
+		                // level 1
+	                    <?php for($x = 0; $x <= $max_downline; $x++) { ?>
+						    <?php if(is_array($downline[$downline_current])) { ?>
+		                    	<?php foreach($downline[$downline_current] as $level) { ?>
+		                    		<?php foreach($customers as $customer) { ?>
+		                    			<?php if($customer['upline_id']==$level) {?>
+				                        	// { id: "<?php echo $customer['id']; ?>", pid: "<?php echo $customer['upline_id']; ?>", Name: "<?php echo $customer['first_name']; ?> <?php echo $customer['last_name']; ?>", Level: "Level <?php echo $level; ?>", email: "<?php echo $customer['email']; ?>", img: "<?php echo $customer['avatar']; ?>" },
+
+				                        	{ id: 2, pid: 1, name: "Lexie Cole", title: "QA Lead", email: "ava@domain.com", img: "https://cdn.balkan.app/shared/2.jpg" },
+				                    		<?php $downline[$downline_next][] = $customer['id']; ?>
+				                    	<?php } ?>
+				                    <?php } ?>
+				                <?php } ?>
+				            <?php } ?>
+				            <?php $downline_current++; ?>
+				            <?php $downline_next++; ?>
+						<?php } ?>
+
+				        // { id: 2, pid: 1, name: "Lexie Cole", title: "QA Lead", email: "ava@domain.com", img: "https://cdn.balkan.app/shared/2.jpg" },
+				        
+				    ];
 
 				    for (var i = 0; i < nodes.length; i++) {
 				        nodes[i].field_number_children = childCount(nodes[i].id);
