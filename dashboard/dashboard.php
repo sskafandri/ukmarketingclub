cf7c76949b3c934636518414980201d65e5f871d
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-12 16:15:53 +0000

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -2048,42 +2048,46 @@ desired effect
 				}
 			</style>
   			
-  			    <script>
-			        window.onload = function () {
-			            var chart = new OrgChart(document.getElementById("tree"), {
-			                template: "ula",
-			                layout: OrgChart.mixed,
-			                nodeBinding: {
-			                    img_0: "img",
-			                    field_0: "name",
-			                    field_1: "title"
-			                    
-			                },
-			                nodes: [
-			                    { id: "1", name: "Jamie Whittingham", title: "Level 0", email: "jamie.whittingham@gmail.com", img: "https://cdn.balkan.app/shared/1.jpg" },
-			                    { id: "2", pid: "1", name: "Ian Orford", title: "Level 1", email: "ian@ianorford.com", img: "https://cdn.balkan.app/shared/2.jpg" },
-			                    { id: "3", pid: "1", name: "Janae Barrett", title: "Technical Director", img: "https://cdn.balkan.app/shared/3.jpg" },
-			                    { id: "4", pid: "1", name: "Aaliyah Webb", title: "Manager", email: "jay@domain.com", img: "https://cdn.balkan.app/shared/4.jpg" },
-			                    { id: "5", pid: "2", name: "Elliot Ross", title: "QA", img: "https://cdn.balkan.app/shared/5.jpg" },
-			                    { id: "6", pid: "2", name: "Anahi Gordon", title: "QA", img: "https://cdn.balkan.app/shared/6.jpg" },
-			                    { id: "7", pid: "2", name: "Knox Macias", title: "QA", img: "https://cdn.balkan.app/shared/7.jpg" },
-			                    { id: "8", pid: "3", name: "Nash Ingram", title: ".NET Team Lead", email: "kohen@domain.com", img: "https://cdn.balkan.app/shared/8.jpg" },
-			                    { id: "9", pid: "3", name: "Sage Barnett", title: "JS Team Lead", img: "https://cdn.balkan.app/shared/9.jpg" },
-			                    { id: "10", pid: "8", name: "Alice Gray", title: "Programmer", img: "https://cdn.balkan.app/shared/10.jpg" },
-			                    { id: "11", pid: "8", name: "Anne Ewing", title: "Programmer", img: "https://cdn.balkan.app/shared/11.jpg" },
-			                    { id: "12", pid: "9", name: "Reuben Mcleod", title: "Programmer", img: "https://cdn.balkan.app/shared/12.jpg" },
-			                    { id: "13", pid: "9", name: "Ariel Wiley", title: "Programmer", img: "https://cdn.balkan.app/shared/13.jpg" },
-			                    { id: "14", pid: "4", name: "Lucas West", title: "Marketer", img: "https://cdn.balkan.app/shared/14.jpg" },
-			                    { id: "15", pid: "4", name: "Adan Travis", title: "Designer", img: "https://cdn.balkan.app/shared/15.jpg" },
-			                    { id: "16", pid: "4", name: "Alex Snider", title: "Sales Manager", img: "https://cdn.balkan.app/shared/16.jpg" }
-			                ]
-			            });
-			            document.getElementById("selectTemplate").addEventListener("change", function () {
-			                chart.config.template = this.value;
-			                chart.draw();
-			            });
-			        }
-			    </script>
+			    <script>
+		        window.onload = function () {
+		            var chart = new OrgChart(document.getElementById("tree"), {
+		                template: "ula",
+		                layout: OrgChart.mixed,
+		                nodeBinding: {
+		                    img_0: "img",
+		                    field_0: "name",
+		                    field_1: "title"
+		                    
+		                },
+		                nodes: [
+		                    // level 0
+		                    { id: "1", name: "<?php echo $account_details['first_name']; ?> <?php echo $account_details['last_name']; ?>", title: "Level 0", email: "<?php echo $account_details['email']; ?>", img: "<?php echo $account_details['avatar']; ?>" },
+		                    <?php $downline[1][]=$_SESSION['account']['id']; ?>
+		                    
+		                    // level 1
+		                    <?php if(is_array($downline[1])) { ?>
+		                    	<?php foreach($downline[1] as $level_1) { ?>
+		                    		<?php foreach($customers as $customer) { ?>
+		                    			<?php if($customer['upline_id']==$level_1) {?>
+				                        	{ id: "<?php echo $customer['id']; ?>", pid: "1", name: "<?php echo $customer['first_name']; ?> <?php echo $customer['last_name']; ?>", title: "Level 1", email: "<?php echo $customer['email']; ?>", img: "<?php echo $customer['avatar']; ?>" },
+				                    		<?php $downline[2][]=$customer['id']; ?>
+				                    	<?php } ?>
+				                    <?php } ?>
+				                <?php } ?>
+				            <?php } ?>
+
+		                    // { id: "2", pid: "1", name: "Ian Orford", title: "Level 1", email: "ian@ianorford.com", img: "https://cdn.balkan.app/shared/2.jpg" },
+		                    
+
+		                    // { id: "3", pid: "1", name: "Janae Barrett", title: "Technical Director", img: "https://cdn.balkan.app/shared/3.jpg" },
+		                ]
+		            });
+		            document.getElementById("selectTemplate").addEventListener("change", function () {
+		                chart.config.template = this.value;
+		                chart.draw();
+		            });
+		        }
+		    </script>
 
             <div class="content-wrapper">
 				
