98b00a5d1b0a65058ea923691ca5d7908e878613
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-13 12:02:07 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,8 +8,10 @@ echo "UBLO Club Affiliate Network - Update Script v"$VERSION
 
 # bash git update script
 cd /home/ukmarketingclub/public_html >> $LOG
-
 git pull >> $LOG
+
+# set folder permissions
+chmod 777 /home/ukmarketingclub/public_html/dashboard/uploads >> $LOG
 
 echo "Update Complete"
 echo " "
