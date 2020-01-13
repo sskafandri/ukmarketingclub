29e1672c878b300b5811f469474374578f04704b
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-13 12:42:17 +0000

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -2199,7 +2199,7 @@ desired effect
 										<button type="button" class="btn btn-info btn-xs btn-flat" data-toggle="modal" data-target="#view_image_<?php echo $product_image['id']; ?>">
 											<i class="fa fa-eye"></i> 
 										</button>
-										<button type="button" class="btn btn-danger btn-xs btn-flat pull-right" data-toggle="modal" data-target="#view_image_<?php echo $product_image['id']; ?>">
+										<button type="button" class="btn btn-danger btn-xs btn-flat text-right" data-toggle="modal" data-target="#view_image_<?php echo $product_image['id']; ?>">
 											<i class="fa fa-times"></i> 
 										</button>
 									</div>
