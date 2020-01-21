04a2668c33d19bd6a202b8974bb450d12fad6a41
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-21 19:05:53 +0000

    Update dashboard.php

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -468,13 +468,6 @@ desired effect
 			        <div class="pull-left info">
 			          	<p><?php echo $account_details['first_name'].' '.$account_details['last_name']; ?></p>
 			          	<a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-
-			          	<?php if(!empty($account_details['badge']['name'])){ ?>
-			          		<center>
-			          			<img src="badges/<?php echo $account_details['badge']['image']; ?>" alt="<?php echo $account_details['badge']['name']; ?>.'" data-toggle="tooltip" data-placement="bottom" title="<?php echo $account_details['badge']['description']; ?>" width="100%"> <br>
-								<h2><strong><?php echo $account_details['badge']['name']; ?></strong></h2>
-							</center>
-			          	<?php } ?>
 			        </div>
 			    </div>
 			    <!--
@@ -490,6 +483,13 @@ desired effect
 			    </form>
 				-->
                 <ul class="sidebar-menu">
+                	<?php if(!empty($account_details['badge']['name'])){ ?>
+		          		<center>
+		          			<img src="badges/<?php echo $account_details['badge']['image']; ?>" alt="<?php echo $account_details['badge']['name']; ?>.'" data-toggle="tooltip" data-placement="bottom" title="<?php echo $account_details['badge']['description']; ?>" width="100%"> <br>
+							<h2><strong><?php echo $account_details['badge']['name']; ?></strong></h2>
+						</center>
+		          	<?php } ?>
+		          	
                 	<?php if($account_details['email'] == 'jamie.whittingham@gmail.com') { ?>
                 		<li class="header">DEV NAVIGATION</li>
 	                    <?php if(get('c') == 'test'){ ?>
