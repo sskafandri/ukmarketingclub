41c190e6e4aebf02ec1bba13baeee68c1d78b7b7
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-27 23:06:22 +0000

    a

--- aff.php
+++ aff.php
@@ -15,24 +15,7 @@ include('/home/ukmarketingclub/public_html/dashboard/inc/global_vars.php');
 include('/home/ukmarketingclub/public_html/dashboard/inc/functions.php');
 
 // get affiliate username
-$username 		= get('username');
-
-debug($_GET);
-die();
-
-// convert username to userid
-// $query      	= $conn->query("SELECT `id` FROM `users` WHERE `affiliate_username` = '".$username."' ");
-// $user      	 	= $query->fetch(PDO::FETCH_ASSOC);
-
-// convert userid to affiliateid
-// $query      	= $conn->query("SELECT `id` FROM `whmcs`.`tblaffiliates` WHERE `clientid` = '".$user['id']."' ");
-// $affiliate 		= $query->fetch(PDO::FETCH_ASSOC);
-
-if(isset($affiliate['id'])){
-	$_SESSION['ref'] = $username;
-}
-
-$_SESSION['ref'] = $username;
+$_SESSION['ref'] 		= get('username');
 
 header("Location: https://ukmarketingclub.com");
 ?>
