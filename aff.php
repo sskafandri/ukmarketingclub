58b148e18eb096f6818b34107b5144c8810a9d50
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-27 22:55:03 +0000

    a

--- aff.php
+++ aff.php
@@ -1,14 +1,13 @@
 <?php
+date_default_timezone_set('UTC');
+
+session_start();
 
 if(isset($_GET['dev']) && $_GET['dev'] == 'yes'){
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
 	ini_set('error_reporting', E_ALL);
 }
-
-date_default_timezone_set('UTC');
-
-session_start();
 
 // includes
 include('/home/ukmarketingclub/public_html/dashboard/inc/db.php');
@@ -19,12 +18,12 @@ include('/home/ukmarketingclub/public_html/dashboard/inc/functions.php');
 $username 		= get('username');
 
 // convert username to userid
-$query      	= $conn->query("SELECT `id` FROM `users` WHERE `affiliate_username` = '".$username."' ");
-$user      	 	= $query->fetch(PDO::FETCH_ASSOC);
+// $query      	= $conn->query("SELECT `id` FROM `users` WHERE `affiliate_username` = '".$username."' ");
+// $user      	 	= $query->fetch(PDO::FETCH_ASSOC);
 
 // convert userid to affiliateid
-$query      	= $conn->query("SELECT `id` FROM `whmcs`.`tblaffiliates` WHERE `clientid` = '".$user['id']."' ");
-$affiliate 		= $query->fetch(PDO::FETCH_ASSOC);
+// $query      	= $conn->query("SELECT `id` FROM `whmcs`.`tblaffiliates` WHERE `clientid` = '".$user['id']."' ");
+// $affiliate 		= $query->fetch(PDO::FETCH_ASSOC);
 
 if(isset($affiliate['id'])){
 	$_SESSION['ref'] = $username;
