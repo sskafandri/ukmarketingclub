5927da4bb29d2cbd81b25889caa6836b054a8b13
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-29 13:20:15 +0000

    added cookie

--- aff.php
+++ aff.php
@@ -16,6 +16,9 @@ include('/home/ukmarketingclub/public_html/dashboard/inc/functions.php');
 
 // get affiliate username
 $_SESSION['affiliate_username'] 		= get('username');
+$cookie_name 							= "affiliate_username";
+$cookie_value 							= get('username');
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 
 header("Location: https://ukmarketingclub.com/");
 ?>
