6e7302198b32af99dc9647935eca01cf49207b9c
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-30 14:57:38 +0000

    a

--- aff.php
+++ aff.php
@@ -1,21 +1,8 @@
 <?php
 session_start();
 
-date_default_timezone_set('UTC');
-
-if(isset($_GET['dev']) && $_GET['dev'] == 'yes'){
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
-	ini_set('error_reporting', E_ALL);
-}
-
-// includes
-include('/home/ukmarketingclub/public_html/dashboard/inc/db.php');
-include('/home/ukmarketingclub/public_html/dashboard/inc/global_vars.php');
-include('/home/ukmarketingclub/public_html/dashboard/inc/functions.php');
-
 // get affiliate username
-$_SESSION['affiliate_username'] 		= get('username');
+$_SESSION['affiliate_username'] 		= $_GET['username'];
 
 header("HTTP/1.1 301 Moved Permanently");
 header("Location: https://ukmarketingclub.com/",true,301);
